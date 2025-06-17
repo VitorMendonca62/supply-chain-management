16f36068934e7b95e64fd4e5b1a6ea076494b44c
Arthur Santos Marinho de Souza <58530401+ASMdeS@users.noreply.github.com>
2025-06-17 07:50:47 -0300

    Apenas uma classe

--- src/Warehouse Management/Estoque.cpp
+++ src/Warehouse Management/Estoque.cpp
@@ -1,62 +1,21 @@
 /******************************************************************************
  * Arquivo     : Estoque.cpp
- * Autor       : Gemini
+ * Autor       : Arthur Marinho
  * Criado em   : 17/06/2025
- * Descrição   : Arquivo de implementação para as classes Estoque e ItemEstoque.
+ * Descrição   : Arquivo de implementação para a classe Estoque (versão refatorada).
  ******************************************************************************/
 
 #include "Estoque.h"
-#include <string>
-#include <vector>
-
-// --- Definições dos Métodos da Classe ItemEstoque ---
-
-bool ItemEstoque::init(int id, int idProd, int idEst, int qtd, const std::string& data) {
-    id_item_estoque = id;
-    id_produto = idProd;
-    id_estoque = idEst;
-    quantidade = qtd;
-    data_ultima_movimentacao = data;
-    return true;
-}
-
-// Getters
-int ItemEstoque::getIdItemEstoque() const {
-    return id_item_estoque;
-}
-
-int ItemEstoque::getIdProduto() const {
-    return id_produto;
-}
-
-int ItemEstoque::getIdEstoque() const {
-    return id_estoque;
-}
-
-int ItemEstoque::getQuantidade() const {
-    return quantidade;
-}
-
-std::string ItemEstoque::getDataUltimaMovimentacao() const {
-    return data_ultima_movimentacao;
-}
-
-// Setters
-void ItemEstoque::setQuantidade(int nova_qtd) {
-    quantidade = nova_qtd;
-}
-
-void ItemEstoque::setDataUltimaMovimentacao(const std::string& nova_data) {
-    data_ultima_movimentacao = nova_data;
-}
-
 
 // --- Definições dos Métodos da Classe Estoque ---
 
-bool Estoque::init(int id, const std::string& nome, const std::string& localizacao) {
-    id_estoque = id;
-    nome_estoque = nome;
+
+bool Estoque::init(int idEstoque, int idProduto, int qtd, const std::string& localizacao, const std::string& data) {
+    id_estoque = idEstoque;
+    id_produto = idProduto;
+    quantidade = qtd;
     localizacao_estoque = localizacao;
+    data_ultima_movimentacao = data;
     return true;
 }
 
@@ -65,23 +24,31 @@ int Estoque::getIdEstoque() const {
     return id_estoque;
 }
 
-std::string Estoque::getNomeEstoque() const {
-    return nome_estoque;
+int Estoque::getIdProduto() const {
+    return id_produto;
+}
+
+int Estoque::getQuantidade() const {
+    return quantidade;
 }
 
 std::string Estoque::getLocalizacaoEstoque() const {
     return localizacao_estoque;
 }
 
-const std::vector<ItemEstoque>& Estoque::getItems() const {
-    return items;
+std::string Estoque::getDataUltimaMovimentacao() const {
+    return data_ultima_movimentacao;
 }
 
 // Setters
-void Estoque::setNomeEstoque(const std::string& novo_nome) {
-    nome_estoque = novo_nome;
+void Estoque::setQuantidade(int nova_quantidade) {
+    quantidade = nova_quantidade;
 }
 
 void Estoque::setLocalizacaoEstoque(const std::string& nova_localizacao) {
     localizacao_estoque = nova_localizacao;
+}
+
+void Estoque::setDataUltimaMovimentacao(const std::string& nova_data) {
+    data_ultima_movimentacao = nova_data;
 }
