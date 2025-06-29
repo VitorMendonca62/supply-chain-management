1cfd01159521e8d99db52c434f27c3698694ded4
Vitor Mendonca <vitorqueiroz325@gmail.com>
2025-06-29 16:15:32 -0300

    refact: atalizando Product.h

---
+++ src/product/Product.cpp
@@ -0,0 +1,101 @@
+/******************************************************************************
+ * Arquivo     : Product.cpp
+ * Autor       : Vitor Mendonça
+ * Criado em   : 15/06/2025
+ * Descrição   : Implementação da classe Product, referente ao produto
+ *
+ * Projeto     : Projeto prático de EDOO
+ *
+ * Histórico de Modificações:
+ *  - 15/06/2025: Versão inicial (Vitor)
+ ******************************************************************************/
+
+#include "Product.h"
+
+// Constructor
+Product::Product(
+    const int id,
+    const std::string &name,
+    const std::string &description,
+    const int weigth,
+    const std::string &unit_measure)
+    : _id(id),
+      _name(name),
+      _description(description),
+      _weigth(weigth),
+      _unit_measure(unit_measure) {}
+
+// ID
+class Product
+{
+private:
+    int _id;
+    std::string _name;
+    std::string _description;
+    int _weigth;
+    std::string _unit_measure;
+
+public:
+    // Constructor
+    Product(
+        const int id,
+        const std::string &name,
+        const std::string &description,
+        const int weigth,
+        const std::string &unit_measure)
+        : _id(id),
+          _name(name),
+          _description(description),
+          _weigth(weigth),
+          _unit_measure(unit_measure) {}
+
+    // ID
+    int get_id() const
+    {
+        return _id;
+    }
+
+    // Name
+    std::string get_name() const
+    {
+        return _name;
+    }
+
+    void set_name(const std::string &name)
+    {
+        _name = name;
+    }
+
+    // Description
+    std::string get_description() const
+    {
+        return _description;
+    }
+
+    void set_description(const std::string &description)
+    {
+        _description = description;
+    }
+
+    // Weigth
+    int get_weigth() const
+    {
+        return _weigth;
+    }
+
+    void set_weigth(int weigth)
+    {
+        _weigth = weigth;
+    }
+
+    // Unit of measure
+    std::string get_unitMeasure() const
+    {
+        return _unit_measure;
+    }
+
+    void set_unitMeasure(const std::string &unit_measure)
+    {
+        _unit_measure = unit_measure;
+    }
+};
--- src/product/Product.h
+++ src/product/Product.h
@@ -3,82 +3,43 @@
  * Autor       : Vitor Mendonça
  * Criado em   : 15/06/2025
  * Descrição   : Criação do arquivo header para a classe Productor, referente ao produto
- * 
+ *
  * Projeto     : Projeto prático de EDOO
- * 
+ *
  * Histórico de Modificações:
  *  - 15/06/2025: Versão inicial (Vitor)
  ******************************************************************************/
-
 
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
 #include <iostream>
 
-
-class Product {
+class Product
+{
 private:
-    int _id;
-    std::string _name;
-    std::string _description;
-    int _weigth;
-    std::string _unit_measure;
+    int id;
+    std::string name;
+    std::string description;
+    int weigth;
+    std::string unit_measure;
 
 public:
-    // Constructor 
-    Product(
-      const int id,
-      const std::string& name,
-      const std::string& description,
-      const int weigth,
-      const std::string& unit_measure)
-      :
-      _id(id),
-      _name(name),
-      _description(description),
-      _weigth(weigth),
-      _unit_measure(unit_measure) {}
+    // Construtor
+    Product(const int id, const std::string &name, const std::string &description, const int weigth, const std::string &unit_measure);
 
-    // ID
-    int get_id() const {
-        return _id;
-    }
+    // Getters
+    int getId() const;
+    std::string getName() const;
+    std::string getDescription() const;
+    int getWeigth() const;
+    std::string getUnitMeasure() const;
 
-    // Name
-    std::string get_name() const {
-        return _name;
-    }
-
-    void set_name(const std::string& name) {
-        _name = name;
-    }
-
-    // Description
-    std::string get_description() const {
-        return _description;
-    }
-
-    void set_description(const std::string& description) {
-        _description = description;
-    }
-
-    // Weigth
-    int get_weigth() const {
-        return _weigth;
-    }
-
-    void set_weigth(int weigth) {
-        _weigth = weigth;
-    }
-
-    // Unit of measure
-    std::string get_unitMeasure() const {
-        return _unit_measure;
-    }
-
-    void set_unitMeasure(const std::string& unit_measure) {
-        _unit_measure = unit_measure;
-    }
+    // Setters
+    void setName(const std::string &name);
+    void setDescription(const std::string &description);
+    void setWeigth(int weigth);
+    void setUnitMeasure(const std::string &unit_measure);
 };
+
 #endif
